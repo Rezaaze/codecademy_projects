8b96c28f9a489f73322355600ba438d306b725a9
Rezaaze <khani.ali54@googlemail.com>
2021-06-30 13:42:18 +0200

    finaly corrected the false syntax in function

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,18 +1,18 @@
 //First generating an object with all the values and attributes needed
 const mixedMessage = {
     
-    startWords :['I','You','He','She','We','They'],
-    midPartOneWords :['should','must','can'],
-    midPartTwoWords :['eat','drive','run','believe','dance','think','learn','laugh'],
-    endWords :['more','faster','less'],
-    wordsList :[this.startWords,this.midPartOneWords,this.midPartTwoWords,this.endWords],
-
+    wordsList:  {
+                startWords :['I','You','He','She','We','They'],
+                midPartOneWords :['should','must','can'],
+                midPartTwoWords :['eat','drive','run','believe','dance','think','learn','laugh'],
+                endWords :['more','faster','less']
+                },
     generateRandomMessage(){
         let message = '';
-    for (key in this.wordsList){
-        message += this.wordsList[key][Math.floor(Math.random()* this.wordsList[key].length] +' ';
-    }
-    return console.log(message); 
+        for (key in this.wordsList){
+            message += this.wordsList[key][Math.floor(Math.random()* this.wordsList[key].length)] +' ';
+        }
+        return console.log(message); 
     }
 
 }
